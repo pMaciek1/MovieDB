a92b6f168b699d9a48bce5560236b6462fc78ff7
pMaciek1 <maciej.plachta1@gmail.com>
2025-03-22 17:57:37 +0100

    Added details for movie

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ db = SQLAlchemy(app)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    release = db.Column(db.String(15))
+    imdbid = db.Column(db.String(10), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Movie %r>' % self.id
 
+with app.app_context():
+    db.create_all()
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     movies = Movie.query.order_by(Movie.date_created).all()
-    return render_template('index.html', movies=movies)
+    return render_template('index.html', movies=movies, previous=request.referrer)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -33,17 +38,17 @@ def delete(id):
         return 'Error, movie not deleted'
 
 @app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
+def update(id): #to be used later
     movie = Movie.query.get_or_404(id)
     if request.method == 'POST':
-        movie.content = request.form['content']
+        movie.title = request.form['content']
         try:
             db.session.commit()
             return redirect('/')
         except:
             return 'Error, movie not updated'
     else:
-        return render_template('update.html', movie=movie)
+        return render_template('update.html', movie=movie, previous=request.referrer)
 
 @app.route('/add/', methods=['POST', 'GET'])
 def add():
@@ -63,19 +68,26 @@ def search():
     for movie in response_list.json()['Search']:
         movie_list.append(movie)
     #return movie_list
-    return render_template('search.html', movie_list=movie_list)
+    return render_template('search.html', movie_list=movie_list, previous=request.referrer)
 
 @app.route('/add/add/<string:imdbID>', methods=['POST', 'GET'])
 def add_movie(imdbID):
     response = requests.get(f'http://www.omdbapi.com/?i={imdbID}&apikey={apikey}')
     title = response.json()['Title']
-    movie = Movie(content=title)
+    release_date = response.json()['Released']
+    movie = Movie(title=title, imdbid=imdbID, release=release_date)
     try:
         db.session.add(movie)
         db.session.commit()
         return redirect('/')
     except:
         return 'Error, movie no added'
+
+@app.route('/details/<string:imdbID>')
+def details(imdbID):
+    response = requests.get(f'http://www.omdbapi.com/?i={imdbID}&apikey={apikey}')
+    movie = response.json()
+    return render_template('details.html', movie=movie, previous=request.referrer)
 
 if __name__ == '__main__':
     app.run(debug=True)
