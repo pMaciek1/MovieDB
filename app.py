c4c41930907ff5113dbd0dac6d86ae4a5fc2e971
pMaciek1 <maciej.plachta1@gmail.com>
2025-03-23 18:57:54 +0100

    Changed styling, added list to watch/watched

--- app.py
+++ app.py
@@ -13,25 +13,36 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     release = db.Column(db.String(15))
+    watched = db.Column(db.Boolean, nullable=False)
     imdbid = db.Column(db.String(10), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Movie %r>' % self.id
 
-with app.app_context():
-    db.create_all()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    movies = Movie.query.order_by(Movie.date_created).all()
+    #movies = Movie.query.order_by(Movie.date_created).all()
+    movies = []
+    for movie in Movie.query.order_by(Movie.date_created).all():
+        if movie.watched:
+            movies.append(movie)
+    return render_template('index.html', movies=movies, previous=request.referrer)
+
+@app.route('/watched/')
+def index_watched():
+    movies = []
+    for movie in Movie.query.order_by(Movie.date_created).all():
+        if not movie.watched:
+            movies.append(movie)
     return render_template('index.html', movies=movies, previous=request.referrer)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Movie.query.get_or_404(id)
+    movie_to_delete = Movie.query.get_or_404(id)
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(movie_to_delete)
         db.session.commit()
         return redirect('/')
     except:
@@ -75,19 +86,29 @@ def add_movie(imdbID):
     response = requests.get(f'http://www.omdbapi.com/?i={imdbID}&apikey={apikey}')
     title = response.json()['Title']
     release_date = response.json()['Released']
-    movie = Movie(title=title, imdbid=imdbID, release=release_date)
+    movie = Movie(title=title, imdbid=imdbID, release=release_date, watched=False)
     try:
         db.session.add(movie)
         db.session.commit()
         return redirect('/')
     except:
-        return 'Error, movie no added'
+        return 'Error, movie not added'
 
 @app.route('/details/<string:imdbID>')
 def details(imdbID):
     response = requests.get(f'http://www.omdbapi.com/?i={imdbID}&apikey={apikey}')
     movie = response.json()
     return render_template('details.html', movie=movie, previous=request.referrer)
+
+@app.route('/watch/<int:id>', methods=['POST', 'GET'])
+def watch(id):
+    movie = Movie.query.get_or_404(id)
+    movie.watched = 1
+    try:
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error, movie not deleted'
 
 if __name__ == '__main__':
     app.run(debug=True)
